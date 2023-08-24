d618f1e9fab92da80a6cc460adfeaa799a8f6362
Luccas Ribeiro Cruz <luccas_185@hotmail.com>
2023-08-24 19:24:34 -0300

    feat: add translate for pt-br

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,28 @@
 const buttonOpenModalExperience = document.querySelector('#buttonExperience')
+const buttonOpenModalExperiencePtBr = document.querySelector('#buttonExperiencePtBr')
 const dialogModalExperience = document.querySelector("#dialogModalExperience")
+const dialogModalExperiencePtBr = document.querySelector("#dialogModalExperiencePtBr")
 const buttonExperienceCloseX = document.querySelector("#buttonExperienceCloseX")
+const buttonExperiencePtBrCloseX = document.querySelector("#buttonExperiencePtBrCloseX")
 
 const buttonOpenModalProjects = document.querySelector('#buttonPrjects')
+const buttonOpenModalProjectsPtBr = document.querySelector('#buttonPrjectsPtBr')
 const dialogModalProjects = document.querySelector("#dialogModalProjects")
+const dialogModalProjectsPtBr = document.querySelector("#dialogModalProjectsPtBr")
 const buttonProjectsCloseX = document.querySelector("#buttonProjectsCloseX")
+const buttonProjectsPtBrCloseX = document.querySelector("#buttonProjectsPtBrCloseX")
 
 
 window.addEventListener("click", (event) => {
     if(event.target == dialogModalProjects || event.target == dialogModalExperience) {
         dialogModalProjects.close()
         dialogModalExperience.close()
+    }
+})
+window.addEventListener("click", (event) => {
+    if(event.target == dialogModalProjectsPtBr || event.target == dialogModalExperiencePtBr) {
+        dialogModalProjectsPtBr.close()
+        dialogModalExperiencePtBr.close()
     }
 })
 
@@ -25,6 +37,18 @@ buttonOpenModalExperience.addEventListener("click", () => {
 })
 buttonExperienceCloseX.addEventListener("click", () => dialogModalExperience.close())
 
+buttonOpenModalExperiencePtBr.addEventListener("click", () => {
+    if (typeof dialogModalExperiencePtBr.showModal === 'function') {
+        dialogModalExperiencePtBr.showModal()
+    } else {
+        alert(
+            "The dialog HTML5 API is not supported by this browser. Please, update."
+        )
+    }
+})
+buttonExperiencePtBrCloseX.addEventListener("click", () => dialogModalExperiencePtBr.close())
+
+
 buttonOpenModalProjects.addEventListener("click", () => {
     if (typeof dialogModalProjects.showModal === 'function') {
         dialogModalProjects.showModal()
@@ -35,3 +59,14 @@ buttonOpenModalProjects.addEventListener("click", () => {
     }
 })
 buttonProjectsCloseX.addEventListener("click", () => dialogModalProjects.close())
+
+buttonOpenModalProjectsPtBr.addEventListener("click", () => {
+    if (typeof dialogModalProjectsPtBr.showModal === 'function') {
+        dialogModalProjectsPtBr.showModal()
+    } else {
+        alert(
+            "The dialog HTML5 API is not supported by this browser. Please, update."
+        )
+    }
+})
+buttonProjectsPtBrCloseX.addEventListener("click", () => dialogModalProjectsPtBr.close())
